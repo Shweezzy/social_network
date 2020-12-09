2ca67db587b75aec09167bed1156ffbb234d08fe
Юра <56113725+Shweezzy@users.noreply.github.com>
2020-12-09 20:54:07 +0200

    add auth, ability to change password and name, find a user

---
+++ backend/authentication/auth.js
@@ -0,0 +1,18 @@
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
+//беремо токен з запроса і зрівнюємо з токеном config.get('jsonwebtoken)
+//кожен користувач має токен з id
+module.exports = (req, res, next) => {
+    const token = req.header('authentication-token');
+    const decode = jwt.verify(
+        token,
+        config.get('jsonwebtoken')
+    );
+
+    //якщо співпадають - користувач декодується
+    //залишається тільки id -users.js(let payload = { user: {id: newUser.id}};)
+
+    req.user = decode.user;
+    next();
+};
